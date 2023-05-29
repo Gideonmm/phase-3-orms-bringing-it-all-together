0c13c18edd8f4c04ef89483d9325f4983fa87d7f
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-29 20:12:08 +0300

    modified initialize method

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,3 +7,14 @@ class Dog
     end
   end
   
+  class Dog
+    attr_accessor :id, :name, :breed
+  
+    def initialize(name:, breed:, id: nil)
+      @id = id
+      @name = name
+      @breed = breed
+    end
+  end
+  
+  
